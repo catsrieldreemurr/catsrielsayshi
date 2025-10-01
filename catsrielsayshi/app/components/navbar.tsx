b99903b845d281382e03855d7e77da273e711e57
Vultus Limunaria <danobrovold@gmail.com>
2025-10-01 18:41:25 +0200

    Add Frontpage box

--- catsrielsayshi/app/components/navbar.tsx
+++ catsrielsayshi/app/components/navbar.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 
 function Navbar(){
     return(
-    <nav className="bg-sky-800 p-3">
+    <nav className="bg-sky-700 p-5">
         <ul className="flex items-center gap-5">
-            <li><Image src="/catsrieldreemurr.png" alt="catsrieldreemurr" height={200} width={200}></Image></li>
-            <li> <Link href="/" className="hover:bg-sky-700 p-3 rounded-sm text-xl">Home</Link> </li>
-            <li> <Link href="/" className="hover:bg-sky-700 p-3 rounded-sm text-xl">Not Home</Link></li>
+            <li> <Link href="https://youtube.com/@CatsrielDreemurr"> <Image src="/catsrieldreemurr.png" alt="catsrieldreemurr" height={200} width={200}></Image></Link></li>
+            <li> <Link href="/" className="hover:bg-sky-800 p-5 rounded-sm text-2xl">Home</Link> </li>
+            <li> <Link href="/" className="hover:bg-sky-800 p-5 rounded-sm text-2xl">Not Home</Link></li>
         </ul>
     </nav>
     )
--- catsrielsayshi/app/page.tsx
+++ catsrielsayshi/app/page.tsx
@@ -5,6 +5,16 @@ export default function Home() {
   return (
     <div>
       <Navbar></Navbar>
+      <div className="bg-[url(/doorsroom0.png)] bg-cover bg-scroll">
+
+      <div className="flex flex-col justify-center items-center h-screen">
+        <div className="bg-black/90 p-10 rounded-2xl ">
+          <h1 className="text-6xl font-bold">YourLocalFern</h1>
+          <p className="text-2xl text-center">I do stuff sometimes</p>
+        </div>
+      </div>
+
     </div>
+  </div>  
   );
 }
